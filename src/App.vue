f11a9e9799da3d5cf65a97f9c327d5ccd72826f8
Light_Quanta <zch22248@foxmail.com>
2024-10-09 03:08:49 +0800

    feat: 添加unplugin-vue-components依赖

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from "./components/Greet.vue";
+
 </script>
 
 <template>
@@ -10,19 +8,19 @@ import Greet from "./components/Greet.vue";
 
     <div class="row">
       <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
+        <img src="/vite.svg" class="logo vite" alt="Vite logo"/>
       </a>
       <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
+        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo"/>
       </a>
       <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo"/>
       </a>
     </div>
 
     <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
 
-    <Greet />
+    <Greet/>
   </div>
 </template>
 
@@ -111,6 +109,7 @@ button {
 button:hover {
   border-color: #396cd8;
 }
+
 button:active {
   border-color: #396cd8;
   background-color: #e8e8e8;
@@ -140,6 +139,7 @@ button {
     color: #ffffff;
     background-color: #0f0f0f98;
   }
+
   button:active {
     background-color: #0f0f0f69;
   }
