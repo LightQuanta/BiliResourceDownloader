126239095319922c8bcd751a9af72f7b1055cfee
Light_Quanta <zch22248@foxmail.com>
2024-10-14 09:40:00 +0800

    feat: 实现装扮和收藏集链接的自动识别和跳转

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Search } from '@element-plus/icons-vue'
+import { GarbSearchResult } from "../types.ts";
 
 const router = useRouter()
 
@@ -13,7 +14,7 @@ const processedInputText = computed(() => {
     if (url.pathname.endsWith('/')) {
       url.pathname = url.pathname.substring(0, url.pathname.length - 1)
     }
-    return `${url.protocol}//${url.host}${url.pathname}`
+    return `${url.protocol}//${url.host}${url.pathname}${url.search}`
   }
   return inputText.value.trim()
 })
@@ -21,16 +22,41 @@ const selectedSearchType = ref('auto')
 
 const getInferredType = (text: string) => {
   if (URL.canParse(text)) {
-    if (/^https:\/\/live\.bilibili\.com\/\d+$/.test(text)) {
+    const url = new URL(text)!
+    if (/^https:\/\/live\.bilibili\.com\/\d+(\?.+)?$/.test(text)) {
       return 'liveroom'
-    } else if (/^https:\/\/space\.bilibili\.com\/\d+$/.test(text) || /^UID:\d+$/.test(text)) {
+    } else if (/^https:\/\/space\.bilibili\.com\/\d+(\?.+)?$/.test(text) || /^UID:\d+$/.test(text)) {
       return 'user'
-    } else if (/^https:\/\/t\.bilibili\.com\/\d+$/.test(text)
-        || /^https:\/\/(www\.)?bilibili\.com\/opus\/\d+$/.test(text)
+    } else if (/^https:\/\/t\.bilibili\.com\/\d+(\?.+)?$/.test(text)
+        || /^https:\/\/(www\.)?bilibili\.com\/opus\/\d+(\?.+)?$/.test(text)
     ) {
       return 'dynamic'
-    } else if (/^https:\/\/(www\.)?bilibili\.com\/(video\/)?((av|AV)\d+|BV\w+)$/.test(text)) {
+    } else if (/^https:\/\/(www\.)?bilibili\.com\/(video\/)?((av|AV)\d+|BV\w+)(\?.+)?$/.test(text)) {
       return 'video'
+    } else if ('https://www.bilibili.com/blackboard/activity-Mz9T5bO5Q3.html' === text.split('?')[0]) {
+      const id = url.searchParams.get('id')
+      const type = url.searchParams.get('type')
+      if (id != null && type != null) {
+        if (type === 'suit') {
+          return 'suit'
+        } else if (type === 'dlc') {
+          return 'lottery'
+        }
+      }
+      return null
+    } else if (text.split('?')[0] === 'https://www.bilibili.com/h5/mall/digital-card/home') {
+      const actId = url.searchParams.get('act_id')
+      const lotteryId = url.searchParams.get('lottery_id')
+      if (actId != null && lotteryId != null) {
+        return 'lottery'
+      }
+      return null
+    } else if (text.split('?')[0] === 'https://www.bilibili.com/h5/mall/suit/detail') {
+      const id = url.searchParams.get('id')
+      if (id != null) {
+        return 'suit'
+      }
+      return null
     }
   } else if (/^((av|AV)\d+|BV\w+)$/.test(text)) {
     return 'video'
@@ -45,7 +71,7 @@ const onChange = () => {
   }
 }
 
-const jump = () => {
+const jump = async () => {
   if (processedInputText.value === '') {
     ElMessage({
       message: '请输入要搜索的内容！',
@@ -67,9 +93,7 @@ const jump = () => {
       })
       return
     }
-  }
-
-  if (type === 'garbSearch') {
+  } else if (type === 'garbSearch') {
     router.push({ path: '/search/garb', query: { keyword: processedInputText.value } })
   } else if (type === 'user') {
     let uid = ''
@@ -77,7 +101,7 @@ const jump = () => {
       if (URL.canParse(processedInputText.value)) {
         uid = URL.parse(processedInputText.value)!.pathname
       } else {
-        uid = processedInputText.value.substring(4)
+        uid = processedInputText.value.substring(4).split('?')[0]
       }
     } else if (/^\d+$/.test(processedInputText.value)) {
       uid = processedInputText.value
@@ -93,7 +117,7 @@ const jump = () => {
   } else if (type === 'liveroom') {
     let roomId = ''
     if (getInferredType(processedInputText.value) === 'liveroom') {
-      roomId = URL.parse(processedInputText.value)!.pathname.substring(1)
+      roomId = URL.parse(processedInputText.value)!.pathname.substring(1).split('?')[0]
     } else if (/^\d+$/.test(processedInputText.value)) {
       roomId = processedInputText.value
     } else {
@@ -113,10 +137,10 @@ const jump = () => {
       return
     }
     let id = ''
-    if (/^https:\/\/t\.bilibili\.com\/\d+$/.test(processedInputText.value)) {
-      id = URL.parse(processedInputText.value)!.pathname.substring(1)
+    if (/^https:\/\/t\.bilibili\.com\/\d+(\?.+)?$/.test(processedInputText.value)) {
+      id = URL.parse(processedInputText.value)!.pathname.substring(1).split('?')[0]
     } else {
-      id = URL.parse(processedInputText.value)!.pathname.substring(6)
+      id = URL.parse(processedInputText.value)!.pathname.substring(6).split('?')[0]
     }
     // TODO 实现动态查看界面
 
@@ -130,8 +154,8 @@ const jump = () => {
     }
 
     let id = ''
-    if (/^https:\/\/(www\.)?bilibili\.com\/(video\/)?((av|AV)\d+|BV\w+)$/.test(processedInputText.value)) {
-      id = URL.parse(processedInputText.value)!.pathname.substring(1)
+    if (/^https:\/\/(www\.)?bilibili\.com\/(video\/)?((av|AV)\d+|BV\w+)(\?.+)?$/.test(processedInputText.value)) {
+      id = URL.parse(processedInputText.value)!.pathname.substring(1).split('?')[0]
       if (id.startsWith('video/')) {
         id = id.substring(6)
       }
@@ -139,6 +163,61 @@ const jump = () => {
       id = processedInputText.value
     }
     // TODO 实现视频查看界面
+  } else if (type === 'lottery') {
+    if (getInferredType(processedInputText.value) !== 'lottery') {
+      ElMessage({
+        message: '请输入正确的收藏集链接！',
+        type: 'error',
+      })
+      return
+    }
+
+    const url = URL.parse(processedInputText.value)!
+
+    const actId = url.searchParams.get('id') ?? url.searchParams.get('act_id')
+    const resp = await fetch(`https://api.bilibili.com/x/vas/dlc_act/act/basic?act_id=${actId}`).then(r => r.json())
+
+    if (resp.code !== 0) {
+      ElMessage({
+        message: '获取收藏集信息出错：' + resp.msg,
+        type: 'error',
+      })
+    }
+
+    const lotteryId = resp.data.tab_lottery_id
+    const lotteryInfo = resp.data.lottery_list.filter(l => l.lottery_id === lotteryId)[0]
+
+    const fakeSearchResult: GarbSearchResult = {
+      item_id: 0,
+      name: resp.data.act_title,
+      jump_link: `https://www.bilibili.com/h5/mall/digital-card/home?act_id=${actId}&lottery_id=${lotteryId}`,
+      sale_count_desc: '',
+      properties: {
+        dlc_act_id: actId as number,
+        dlc_lottery_id: lotteryId as number,
+
+        dlc_lottery_sale_quantity: lotteryInfo.total_sale_amount as number,
+        image_cover: lotteryInfo.lottery_image as string,
+
+        dlc_sale_start_time: lotteryInfo.start_time as string,
+        dlc_sale_end_time: lotteryInfo.end_time as string,
+      }
+    }
+
+    router.push({ path: '/lottery', query: { lottery: JSON.stringify(fakeSearchResult) } })
+  } else if (type === 'suit') {
+    if (getInferredType(processedInputText.value) !== 'suit') {
+      ElMessage({
+        message: '请输入正确的收藏集链接！',
+        type: 'error',
+      })
+      return
+    }
+
+    const url = URL.parse(processedInputText.value)!
+    const id = url.searchParams.get('id')
+
+    // TODO 实现装扮页面
   }
 }
 </script>
@@ -161,6 +240,8 @@ const jump = () => {
           <ElOption label="用户信息" value="user"/>
           <ElOption label="动态" value="dynamic"/>
           <ElOption label="视频" value="video"/>
+          <ElOption label="收藏集" value="lottery"/>
+          <ElOption label="装扮" value="suit"/>
           <ElOption label="装扮/收藏集搜索" value="garbSearch"/>
         </ElSelect>
       </template>
