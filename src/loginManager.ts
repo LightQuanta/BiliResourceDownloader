df979ba1c28c5874962df176d411d060fcd16842
Light_Quanta <zch22248@foxmail.com>
2024-10-23 20:57:42 +0800

    fix: 优化登录状态保存和读取

--- src/loginManager.ts
+++ src/loginManager.ts
@@ -1,4 +1,5 @@
 import { createStore, Store } from "@tauri-apps/plugin-store";
+import { clearAPICache } from "./APIFetch.ts";
 
 let internalStore: Store | null = null
 
@@ -15,17 +16,18 @@ async function saveLoginCookie(cookie: string) {
     const store = await getLoginStore()
     await store.set('cookie', cookie)
     await store.save()
+    await clearAPICache()
     userLoggedIn.value = true
 }
 
 async function getLoginCookie() {
     const store = await getLoginStore()
-    return await store.get('cookie')
+    return await store.get('cookie') as string
 }
 
 async function checkLoginState() {
     const cookie = await getLoginCookie()
-    userLoggedIn.value = cookie !== null
+    userLoggedIn.value = (cookie?.length ?? 0) > 0
     return userLoggedIn.value
 }
 
@@ -33,6 +35,7 @@ async function clearLoginCookie() {
     const store = await getLoginStore()
     await store.clear()
     await store.save()
+    await clearAPICache()
     userLoggedIn.value = false
 }
 
