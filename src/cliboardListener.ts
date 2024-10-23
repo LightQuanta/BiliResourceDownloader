5a76f071a0e417487f17c649e18810b9b46f1778
Light_Quanta <zch22248@foxmail.com>
2024-10-23 11:02:14 +0800

    feat: 将剪切板缓存存储至store

--- src/cliboardListener.ts
+++ src/cliboardListener.ts
@@ -1,12 +1,20 @@
 import { readText } from "@tauri-apps/plugin-clipboard-manager";
 import { autoJump } from "./linkResolver.ts";
-
-let clipboardCache = ""
+import { createStore, Store } from "@tauri-apps/plugin-store";
 
 // TODO 实现自动跳转对应的配置项
 // const enabled = true
 
 let taskID = 0
+
+let internalStore: Store | null = null
+
+async function getClipboardStore() {
+    if (internalStore === null) {
+        internalStore = await createStore('clipboard')
+    }
+    return internalStore
+}
 
 function startClipboardListening() {
     if (taskID > 0) return
@@ -17,16 +25,19 @@ function startClipboardListening() {
         } catch {
             return
         }
+        const store = await getClipboardStore()
+        const clipboardCache = await store.get('clipboard') as string | undefined
         if (text !== clipboardCache) {
-            clipboardCache = text
+            await store.set('clipboard', text)
             await autoJump(text)
         }
     }, 1000)
 }
 
-function stopClipboardListening() {
+async function stopClipboardListening() {
     clearInterval(taskID)
-    clipboardCache = ""
+    const store = await getClipboardStore()
+    await store.delete('clipboard')
     taskID = -1
 }
 
