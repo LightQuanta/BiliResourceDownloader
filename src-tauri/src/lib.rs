6da77ab9bf3c095cbbe5cb5112cc26f4e46e85a3
Light_Quanta <zch22248@foxmail.com>
2024-10-25 15:55:20 +0800

    feat: 实现SVGA动画解析和下载

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use base64::prelude::BASE64_STANDARD;
+use base64::Engine;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 use std::fs;
 
@@ -10,6 +12,18 @@ fn create_dir(path: &str) -> &str {
     "ok"
 }
 
+#[tauri::command]
+fn save_data_url<'a>(path: &'a str, data: &'a str) -> String {
+    let base64 = data.split(',').collect::<Vec<&str>>()[1];
+    let bytes = BASE64_STANDARD.decode(base64).unwrap_or(vec![]);
+
+    let result = fs::write(path, bytes);
+    if let Err(e) = result {
+        return e.to_string();
+    }
+    "ok".to_string()
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -19,7 +33,7 @@ pub fn run() {
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![create_dir])
+        .invoke_handler(tauri::generate_handler![create_dir, save_data_url])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/SVGACard.vue
+++ src/components/SVGACard.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import { Parser, Player } from "svga";
+import { open, save } from "@tauri-apps/plugin-dialog";
+import { download } from "@tauri-apps/plugin-upload";
+import { Video } from "svga/dist/types";
+import { invoke } from "@tauri-apps/api/core";
+import { sep } from "@tauri-apps/api/path";
 
 const props = defineProps<{
   url?: string
@@ -9,7 +14,79 @@ const props = defineProps<{
   downloadName?: string
 }>()
 
-const canvasRef = ref<HTMLCanvasElement>()
+let SVGAVideo: Video
+
+const playerCanvasRef = ref<HTMLCanvasElement>()
+let playerCtx: CanvasRenderingContext2D | undefined
+
+const renderCanvasRef = ref<HTMLCanvasElement>()
+let player: Player
+let renderedFrames: (ImageData | undefined)[]
+let renderedFramesCount = 0
+let totalFrames = 0
+
+// 渲染序列帧图片
+const renderSequence = async () => {
+  await renderFramesData()
+
+  const renderCanvas = renderCanvasRef.value as HTMLCanvasElement
+
+  const { width, height } = SVGAVideo.size
+
+  renderCanvas.height = height
+  renderCanvas.width = width * totalFrames
+
+  const renderCtx = renderCanvas.getContext('2d') as CanvasRenderingContext2D
+  renderCtx.reset()
+
+  for (let i = 0; i < totalFrames; i++) {
+    const data = renderedFrames[i] as ImageData
+    renderCtx.putImageData(data, i * width, 0)
+  }
+}
+
+// 渲染所有帧图片，返回dataURL[]
+const renderSequenceImages = async () => {
+  await renderFramesData()
+
+  const renderCanvas = renderCanvasRef.value as HTMLCanvasElement
+
+  const { width, height } = SVGAVideo.size
+
+  renderCanvas.height = height
+  renderCanvas.width = width
+
+  const renderCtx = renderCanvas.getContext('2d') as CanvasRenderingContext2D
+  renderCtx.reset()
+
+  const images: string[] = []
+  for (let i = 0; i < totalFrames; i++) {
+    const data = renderedFrames[i] as ImageData
+    renderCtx.putImageData(data, 0, 0)
+    images.push(renderCanvas.toDataURL())
+  }
+  return images
+}
+
+// 通过playerCanvas渲染动画每帧数据
+const renderFramesData = async () => {
+  return new Promise<void>(resolve => {
+    player.onProcess = () => {
+      if (renderedFramesCount === totalFrames) {
+        player.onProcess = undefined
+        resolve()
+      }
+
+      const index = player.currentFrame
+
+      if (!renderedFrames?.[index]) {
+        renderedFrames[index] = playerCtx?.getImageData(0, 0, playerCanvasRef.value?.width ?? 0, playerCanvasRef.value?.height ?? 0)
+        renderedFramesCount++
+      }
+    }
+  })
+}
+
 
 const init = async () => {
   const url = props.url
@@ -19,48 +96,107 @@ const init = async () => {
 
   const dataUrl = URL.createObjectURL(blob)
   const parser = new Parser()
-  const svga = await parser.load(dataUrl)
+  SVGAVideo = await parser.load(dataUrl)
 
-  const player = new Player(canvasRef.value as HTMLCanvasElement)
-  await player.mount(svga)
+  player = new Player(playerCanvasRef.value as HTMLCanvasElement)
+  await player.mount(SVGAVideo)
   player.start()
+
+  playerCtx = playerCanvasRef.value?.getContext('2d', { willReadFrequently: true }) ?? undefined
+
+  totalFrames = SVGAVideo.frames - 1
+  renderedFrames = Array<ImageData | undefined>(totalFrames)
+  renderedFramesCount = 0
 }
 
 onMounted(init)
 watch(() => props.url, init)
 
-// TODO 正确实现下载
-// const downloadFile = async (url: string, suffix: string) => {
-//   const name = (props.downloadName ?? props.title) + '.' + suffix
-//
-//   const path = await save({
-//     defaultPath: name,
-//     filters: [
-//       {
-//         name: suffix,
-//         extensions: [suffix],
-//       },
-//     ],
-//   })
-//   if (path === null) return
-//
-//   try {
-//     // await download(url, path, undefined, header)
-//   } catch (e) {
-//     console.error(e)
-//
-//     ElMessage({
-//       message: `下载出错： ${e}`,
-//       type: 'error',
-//     })
-//     return
-//   }
-//
-//   ElMessage({
-//     message: `${name}下载成功`,
-//     type: 'success',
-//   })
-// }
+const saveFile = (name: string, extension: string) => {
+  return save({
+    defaultPath: name,
+    filters: [
+      {
+        name: extension,
+        extensions: [extension],
+      },
+    ],
+  })
+}
+
+const downloadSequenceImages = async () => {
+  const name = props.downloadName ?? props.title + '帧序列'
+
+  const path = await open({
+    defaultPath: name,
+    directory: true,
+  })
+
+  if (path === null) return
+
+  ElMessage('正在渲染帧数据，请稍等')
+  const images = await renderSequenceImages()
+  await invoke('create_dir', { path: `${path}${sep()}${name}` })
+  const results = await Promise.all(images.map((data, index) => invoke('save_data_url', {
+    path: `${path}${sep()}${name}${sep()}图片${index + 1}.png`,
+    data,
+  })))
+
+  if (results.some(r => r !== 'ok')) {
+    ElMessage({
+      message: `保存${name}时出错`,
+      type: "error",
+    })
+  } else {
+    ElMessage({
+      message: `保存${name}成功`,
+      type: "success",
+    })
+  }
+}
+
+const downloadSequence = async () => {
+  const name = props.downloadName ?? props.title + ' - 帧序列'
+  const path = await saveFile(name, 'png')
+  if (path === null) return
+
+  ElMessage('正在渲染帧数据，请稍等')
+
+  await renderSequence()
+  const data = renderCanvasRef.value?.toDataURL() ?? ''
+
+  if (await invoke('save_data_url', { path, data }) === 'ok') {
+    ElMessage({
+      message: `下载${name}成功！`,
+      type: "success",
+    })
+  }
+}
+
+const downloadSVGA = async () => {
+  if (!props.url) return
+
+  const name = props.downloadName ?? props.title ?? 'file'
+  const path = await saveFile(name, 'svga')
+  if (path === null) return
+
+  try {
+    await download(props.url, path)
+  } catch (e) {
+    console.error(e)
+
+    ElMessage({
+      message: `下载出错： ${e}`,
+      type: 'error',
+    })
+    return
+  }
+
+  ElMessage({
+    message: `${name}下载成功`,
+    type: 'success',
+  })
+}
 
 </script>
 
@@ -89,19 +225,39 @@ watch(() => props.url, init)
     </template>
 
     <!-- SVGA播放器 -->
-    <canvas ref="canvasRef" />
+    <canvas ref="playerCanvasRef" />
+
+    <!-- 渲染序列帧用canvas -->
+    <canvas
+      ref="renderCanvasRef"
+      class="hidden"
+    />
 
     <template #footer>
       <div class="flex h-4 items-center justify-center">
-        <!-- TODO 实现对应下载 -->
         <ElButtonGroup>
-          <ElButton type="primary">
+          <ElButton
+            type="primary"
+            @click="downloadSVGA"
+          >
             SVGA图片
             <ElIcon size="20">
               <i-ep-download />
             </ElIcon>
           </ElButton>
-          <ElButton type="primary">
+          <ElButton
+            type="primary"
+            @click="downloadSequenceImages"
+          >
+            每帧图片
+            <ElIcon size="20">
+              <i-ep-download />
+            </ElIcon>
+          </ElButton>
+          <ElButton
+            type="primary"
+            @click="downloadSequence"
+          >
             帧序列
             <ElIcon size="20">
               <i-ep-download />
